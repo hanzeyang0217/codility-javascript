c1b918ea06fbdc6f9379777b2cc59e874345f860
Hanzeyang <hanzeyang0217@gmail.com>
2021-01-13 22:43:13 +0900

    add link

--- 别的/洗牌算法.js
+++ 别的/洗牌算法.js
@@ -1,6 +1,7 @@
 //随机排序
 //结论确实洗牌算法精确些
 //https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A3%E3%83%83%E3%82%B7%E3%83%A3%E3%83%BC%E2%80%93%E3%82%A4%E3%82%A7%E3%83%BC%E3%83%84%E3%81%AE%E3%82%B7%E3%83%A3%E3%83%83%E3%83%95%E3%83%AB
+//https://www.zhihu.com/question/68330851/answer/266506621
 
 Array.prototype.shuffle = function () {
   let m = this.length;
