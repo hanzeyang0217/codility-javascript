65c81d91afc3aebc485ab1dff4ba3db50670ddd4
Hanzeyang <hanzeyang0217@gmail.com>
2021-01-17 22:03:32 +0900

    update

--- 5Prefix Sums/CountDiv.js
+++ 5Prefix Sums/CountDiv.js
@@ -1,8 +1,17 @@
-//正常些速度有问题 直接找规律就好了
+/*
+* 题目莫名其妙 先pass
+**/
 function solution(A, B, K) {
-  let count1 = A / K;
-  Number.isInteger(count1) === true
-    ? (count1 -= 1)
-    : (count1 = Math.trunc(count1));
-  return Math.trunc(B / K) - count1;
+  return Math.floor(B / K) - Math.floor((A - 1) / K)
 }
+
+//正常些速度有问题 直接找规律就好了
+// function solution(A, B, K) {
+//   let count1 = A / K;
+//   Number.isInteger(count1) === true
+//     ? (count1 -= 1)
+//     : (count1 = Math.trunc(count1));
+//   return Math.trunc(B / K) - count1;
+// }
+solution(6, 11, 2) //3
+solution(0, 0, 11) //1
--- 5Prefix Sums/GenomicRangeQuery.js
+++ 5Prefix Sums/GenomicRangeQuery.js
@@ -1,9 +1,37 @@
 /**
- * # 想法1
- * 1. 遍历M次
- * 2. 每次可以获得一个长度截取DNA
- * 3. 截取的DNA里面找到最小数返回
+ * 1/17 20min
  **/
+//不开心没想通 为啥这个费效率...
+function solution(S, P, Q) {
+  const obj = {
+    A: 1,
+    C: 2,
+    G: 3,
+    T: 4,
+  }
+  return P.map((item, i) => Array.from(new Set(Array.from(S).map(i => obj[i]).slice(item, Q[i] + 1))).sort((a, b) => a - b)[0])
+}
+
+//大佬的逼格更高 模仿一下
+// function solution(S, P, Q) {
+//   /**
+//    * # 想法2
+//    * 1. 拿到每段DNA
+//    * 2. 返回每段最小值
+//    **/
+//   let DNA = P.map((item, i) => S.slice(item, Q[i] + 1))
+//   return DNA.map(i => {
+//     if (i.indexOf('A') > -1) {
+//       return 1
+//     } else if (i.indexOf('C') > -1) {
+//       return 2
+//     } else if (i.indexOf('G') > -1) {
+//       return 3
+//     } else if (i.indexOf('T') > -1) {
+//       return 4
+//     }
+//   })
+// }
 
 // function solution(S, P, Q) {
 //   const hesuan = ['A', 'C', 'G', 'T']
@@ -22,28 +50,15 @@
 //   }
 //   return r
 // }
-//大佬的逼格更高 模仿一下
-function solution(S, P, Q) {
-  /**
-   * # 想法2
-   * 1. 拿到每段DNA
-   * 2. 返回每段最小值
-   **/
-  let DNA = P.map((item, i) => S.slice(item, Q[i] + 1))
-  return DNA.map(i => {
-    if (i.indexOf('A') > -1) {
-      return 1
-    } else if (i.indexOf('C') > -1) {
-      return 2
-    } else if (i.indexOf('G') > -1) {
-      return 3
-    } else if (i.indexOf('T') > -1) {
-      return 4
-    }
-  })
-}
 
-solution("CAGCCTA", [2, 5, 0], [4, 5, 6]); // [2, 4, 1]
+solution("CAGCCTA", [2, 5, 0], [4, 5, 6]) // [2, 4, 1]
+//2-4 GCC
+//5-5 T
+//0-6 CAGCCTA
+solution("TC", [0, 0, 1], [0, 1, 1]) // [4, 2, 2]
+//0-0 T
+//0-1 TC
+//1-1 C
 //N 1~100,000
 //M 1~50,000
 //P Q 0 ~ N-1
