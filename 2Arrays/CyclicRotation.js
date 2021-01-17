32d519835c95833f5d22fc823fb63c60822be37b
Hanzeyang <hanzeyang0217@gmail.com>
2021-01-17 10:46:43 +0900

    update CyclicRotation with one line

--- 2Arrays/CyclicRotation.js
+++ 2Arrays/CyclicRotation.js
@@ -1,15 +1,24 @@
 /**
  * 1/11 15min
+ * 1/17 10min
  *
  * # 学到
  * 1. 跟循环有关的用 % 注意 0的时候直接返回
  * 2. splice concat组合估计很好用
+ * # 更新
+ * 1/17
+ * 1. 实现一行解决问题不过可能不太适合阅读
  */
 function solution(A, K) {
-  const count = K % A.length
-  if (count === 0) return A
-  return A.splice(A.length - count).concat(A)
+  //切割原数组从(最后位减去需要移动的个数)到最后(省略)然后拼接起来
+  return A.splice(A.length - (K % A.length)).concat(A)
 }
+
+// function solution(A, K) {
+//   const count = K % A.length
+//   if (count === 0) return A
+//   return A.splice(A.length - count).concat(A)
+// }
 
 solution([3, 8, 9, 7, 6], 3) // [9, 7, 6, 3, 8]
 solution([0, 0, 0], 1) // [0, 0, 0]
