30843f968239dbbaddab1887552e3b82663ef08c
Hanzeyang <hanzeyang0217@gmail.com>
2021-01-18 23:22:05 +0900

    add PassingCars

---
+++ 5Prefix Sums/PassingCars.js
@@ -0,0 +1,38 @@
+//第一版 数据对的 但是O(N ** 2)
+// function solution(A) {
+//   let sum = 0
+//   for (let i = 0; i < A.length; i++) {
+//     if (A[i] === 0) sum += A.slice(i + 1, A.length).filter(a => a === 1).length
+//   }
+//   return sum
+// }
+
+//改良 第一个for跑完以后再跑一个for 试试看
+//天才!!!
+//累加了以后知道1有几个
+//然后就计数嘛
+// function solution(A) {
+//   let sum = A.reduce((a, b) => a + b)
+//   let c = 0
+//   let r = 0
+//   for (let i = 0; i < A.length; i++) {
+//     if (A[i] === 0) r += sum - c
+//     if (A[i] === 1) c++
+//   }
+//   return r > 1000000000 ? -1 : r
+// }
+//还有一种想法反过来的
+//每次的1*还没有到的0的数量就好了
+function solution(A) {
+  let right = 0
+  let count = 0
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === 0) right++
+    if (A[i] === 1) count += right
+  }
+  return count > 1000000000 ? -1 : count
+}
+
+solution([1, 0]) //0
+solution([0, 1, 0, 1, 1]) //5
+//1000000000 -1
