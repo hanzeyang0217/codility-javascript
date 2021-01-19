30dc4c23b51555269fce770e0679c653470eaf7f
Hanzeyang <hanzeyang0217@gmail.com>
2021-01-20 00:50:18 +0900

    add

---
+++ 6Sorting/MaxProductOfThree.js
@@ -0,0 +1,11 @@
+/**
+ * 1/19 15min
+ * 就是正正正或者正负负两种情况
+ * 当然可能实际上不是↑
+ **/
+function solution(A) {
+  A.sort((a, b) => b - a)
+  return Math.max(A[0] * A[1] * A[2], A[0] * A.pop() * A.pop())
+}
+
+solution([-3, 1, 2, -2, 5, 6]) //60
