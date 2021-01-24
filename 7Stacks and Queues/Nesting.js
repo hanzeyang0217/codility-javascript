bf1c6f8a887fe48a011ad179769dd2d76c04ad0a
Hanzeyang <hanzeyang0217@gmail.com>
2021-01-24 16:17:44 +0900

    add

---
+++ 7Stacks and Queues/Nesting.js
@@ -0,0 +1,20 @@
+/**
+ * 跟 Brackets一摸一样...转成数组以后会没时间 直接用字符串遍历叭
+ * 1/24 10min
+ * @param S
+ * @returns {number}
+ */
+function solution(S) {
+  const stack = []
+  for (let i=0; i<S.length; i++) {
+    if (stack[stack.length - 1] + S[i] === '()'){
+      stack.pop()
+    }else{
+      stack.push(S[i])
+    }
+  }
+  return stack.length === 0 ? 1 : 0
+}
+
+solution("(()(())())")//1
+solution("())")//0
