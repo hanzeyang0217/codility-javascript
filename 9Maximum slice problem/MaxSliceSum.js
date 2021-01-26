8a9118170491cfafe584134f5f3ec23f3b0272d2
Hanzeyang <hanzeyang0217@gmail.com>
2021-01-26 11:02:43 +0900

    upd

--- 9Maximum slice problem/MaxSliceSum.js
+++ 9Maximum slice problem/MaxSliceSum.js
@@ -1,6 +1,7 @@
 /**
  * 跟上一题一样 根据逻辑走
  * 1/24 15min
+ * 1/25 30min 需要再练练
  * @param A
  * @returns {*}
  */
@@ -13,6 +14,7 @@ function solution(A) {
   }
   return max
 }
-
+solution([3, 2, -1, 4, 0]); //8
+solution([3, 2, -6, 4, 2]); //5
 // solution([3, 2, -6, 4, 0]) //5
 // solution([5, -4, 3])//5
