40dd6c8dd08bd640635f0f22fb9d56808d02e092
Hanzeyang <hanzeyang0217@gmail.com>
2021-02-01 10:50:34 +0900

    upd

--- 3TimeComplexity/TapeEquilibrium.js
+++ 3TimeComplexity/TapeEquilibrium.js
@@ -1,20 +1,34 @@
 /**
  * 1/11 20min 有问题
  * 1/17 10min 差不多
+ * 2/1  10min 搞定
  * # 问题
  * 1. 最后一刀是不能切的 注意读题目
  * # 学到
  * 1. 累加用reduce
  * 2. 比较用Math.min
  * 3. Infinity用到实战中
+ * 4. 上面都习惯了 yeah
  * 1/17
  * 1. 实现3行解决问题不过可能不太适合阅读
  */
+
 function solution(A) {
-  const sum = A.reduce((a, b) => a + b)
-  let left = 0
-  return A.map(i => Math.abs((left += i) * 2 - sum)).slice(0, -1).sort((a, b) => a - b)[0]
+  let sum = A.reduce((a, b) => a + b)
+  let min = Infinity
+  let leftSum = 0
+
+  for (let i = 0; i < A.length - 1; i++) {
+    min = Math.min(min, Math.abs(sum - (leftSum += A[i]) * 2))
+  }
+  return min
 }
+//最短写法 不过效率嘛w
+// function solution(A) {
+//   const sum = A.reduce((a, b) => a + b)
+//   let left = 0
+//   return A.map(i => Math.abs((left += i) * 2 - sum)).slice(0, -1).sort((a, b) => a - b)[0]
+// }
 
 // function solution(A) {
 //   let totalSum = A.reduce((a, b) => a + b)
